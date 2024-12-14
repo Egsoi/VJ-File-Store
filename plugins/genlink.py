dd8f1f725ff57bc14799ce6e9304f45b9186081b
Egsoi <shaho45540+git@gmail.com>
2024-12-15 01:08:58 +0530

    Update genlink.py

--- plugins/genlink.py
+++ plugins/genlink.py
@@ -39,7 +39,7 @@ import base64
 async def incoming_gen_link(client, message):
     username = (await client.get_me()).username
     file_type = message.media
-    file_id, ref = unpack_new_file_id((getattr(message, file_type.value)).file_id))
+    file_id, ref = unpack_new_file_id((getattr(message, file_type.value)).file_id)
     string = 'file_'
     string += file_id
     outstr = base64.urlsafe_b64encode(string.encode("ascii")).decode().strip("=")
